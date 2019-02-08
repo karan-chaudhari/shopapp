191f1f574ed67fdc645e50275f47b2e59eaf742d
karan-chaudhari <chaudharik353@gmail.com>
2019-02-09 00:32:00 +0530

    Create UrlPatterns in shop app

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='shop'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('productview/', views.productView, name='productView'),
+    path('tracker/', views.tracker, name='tracker'),
+    path('checkout/', views.checkout, name='checkout')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse('About Shop')
+
+def contact(request):
+    return HttpResponse('Contact Shop') 
+
+def productView(request):
+    return HttpResponse('Our Products')
+
+def tracker(request):
+    return HttpResponse('Tracker')
+
+def checkout(request):
+    return HttpResponse('Check Out')
