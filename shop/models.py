dc76dc2664ff5c7affa9841be93ca42d42bad16c
karan-chaudhari <chaudharik353@gmail.com>
2019-02-10 00:32:41 +0530

    Create shop home page

--- shop/models.py
+++ shop/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50)
-    desc = models.CharField(max_length=500)
+    product_cate = models.CharField(max_length=50)
+    img = models.ImageField()
+    price = models.CharField(max_length=20)
+    desc = models.TextField(max_length=1000)
     pub_date = models.DateField()
