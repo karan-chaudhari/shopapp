1bfaad0b6496c3383a44977db58edfb499846978
karan-chaudhari <chaudharik353@gmail.com>
2019-02-15 00:02:06 +0530

    Add products in home page

--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50)
-    product_cate = models.CharField(max_length=50)
-    img = models.ImageField()
-    price = models.CharField(max_length=20)
-    desc = models.TextField(max_length=1000)
+    cate = models.CharField(max_length=50, default="")
+    subcate = models.CharField(max_length=50, default="")
+    img = models.ImageField(upload_to='shop/images',default="")
+    price = models.CharField(max_length=20, default="")
+    desc = models.TextField(max_length=1000, blank=True)
     pub_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'shop/index.html', context)
 
 def about(request):
     return HttpResponse('About Shop')
