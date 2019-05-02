3dc5812e226303e512db3d338fb4cf507299127f
karan-chaudhari <chaudharik353@gmail.com>
2019-05-02 22:15:23 +0530

    Add blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	slug = models.CharField(max_length=100)
+	category = models.CharField(max_length=100, blank=True)
+	timestamp = models.DateTimeField(default=timezone.now)
+	img = models.ImageField(upload_to='blog/images')
+	content = models.TextField(max_length=2000)
+
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from shop.models import Product
+from .models import Post
 
-def index(request):
-    return render(request, 'blog/index.html')
+cateprod = Product.objects.values('cate')
+cates = {item['cate'] for item in cateprod}
+
+def home(request):
+	post = Post.objects.all()[::-1]
+	paginator = Paginator(post,6)
+	page = request.GET.get('page')
+	posts = paginator.get_page(page)
+	context = {'posts':posts,'cate':cates}
+	return render(request, 'blog/home.html', context)
